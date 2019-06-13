bddfd22d0ed7b34c22a6a4c5b99fe98244a18557
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2019-06-13 09:40:52 -0500

    Initial Commit

---
+++ MerlinRecomendations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MerlinRecomendations
+//
+//  Created by David A Cespedes R on 6/13/19.
+//  Copyright © 2019 David A Cespedes R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ MerlinRecomendationsTests/MerlinRecomendationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MerlinRecomendationsTests.swift
+//  MerlinRecomendationsTests
+//
+//  Created by David A Cespedes R on 6/13/19.
+//  Copyright © 2019 David A Cespedes R. All rights reserved.
+//
+
+import XCTest
+@testable import MerlinRecomendations
+
+class MerlinRecomendationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
